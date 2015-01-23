12f0c5c2717d29d9f4b8b265856b2f7e13b73d60
Ilya Bondarenko <bondarenko.ik@gmail.com>
2015-01-23 16:41:25 +0500

    Fix .any? method
    Now method recive hash with array of actions.
    Like {pages:[:new, :edit]}.
    Also save singe {pages: :new} format (like controller filter style)
    Update .gitignore for ignoring .idea folder

--- app/models/concerns/the_role/base.rb
+++ app/models/concerns/the_role/base.rb
@@ -9,14 +9,17 @@ module TheRole
 
     def has_role? section_name, rule_name
       hash         =  role_hash
-      section_name =  section_name.to_slug_param(sep: '_')
-      rule_name    =  rule_name.to_slug_param(sep: '_')
 
       return true  if hash.try(:[], 'system').try(:[], 'administrator')
       return true  if hash.try(:[], 'moderator').try(:[], section_name)
 
-      return false unless hash[section_name]
-      return false unless hash[section_name].key? rule_name
+      section_name =  section_name.to_slug_param(sep: '_')
+      [*rule_name].each do |rule|
+        rule_name    =  rule.to_slug_param(sep: '_')
+
+        return false unless hash[section_name]
+        return false unless hash[section_name].key? rule_name
+      end
       hash[section_name][rule_name]
     end
 
