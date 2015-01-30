3c054ebb6f9f135503e7bd199e7fd8622aef43d9
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-01-30 14:42:00 +0300

    config upd

--- lib/the_role/config.rb
+++ lib/the_role/config.rb
@@ -19,11 +19,11 @@ module TheRole
   end
 
   configure do |config|
-    config.layout                     = :application
+    config.layout = :application
 
     config.default_user_role          = nil
+    config.access_denied_method       = nil
     config.first_user_should_be_admin = false
-    config.access_denied_method       = :access_denied
     config.login_required_method      = :authenticate_user!
     config.destroy_strategy           = :restrict_with_exception # can be nil
   end
