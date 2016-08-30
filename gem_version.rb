42b2d17be6f03419b2cc5bdd8c7496686353f91b
Ilya N. Zykin <zykin-ilya@ya.ru>
2016-08-30 20:52:43 +0300

    v3.5, Rails 5

--- gem_version.rb
+++ gem_version.rb
@@ -1,3 +1,3 @@
 module TheRoleApi
-  VERSION = "3.0.3"
+  VERSION = "3.5"
 end
--- lib/the_role_api.rb
+++ lib/the_role_api.rb
@@ -30,15 +30,9 @@ module TheRole
       %w[ base_methods role user ].each do |file|
         require_dependency "#{ app }/models/concerns/the_role/api/#{ file }.rb"
       end
-    end
-
-    if Rails::VERSION::MAJOR == 4
+    else
       config.autoload_paths << "#{ config.root }/app/models/concerns/**"
       config.autoload_paths << "#{ config.root }/app/controllers/concerns/**"
-    end
-
-    if Rails::VERSION::MAJOR == 5
-      raise Exception.new("TheRole 3. Version for Rails 5 not tested yet")
     end
 
     initializer "the_role_precompile_hook", group: :all do |app|
